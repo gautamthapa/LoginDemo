b6c3c9056f29ea4fdc33a16de164d7a72e0dc259
Gautam <gautam.thapa22@gmail.com>
2017-11-13 15:35:09 +0530

    Today i design all the basic UI of the app.

---
+++ app/src/main/java/com/imstar/gautam/logindemo/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.imstar.gautam.logindemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gautam on 13-11-2017.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_profile);
+    }
+}
---
+++ app/src/main/java/com/imstar/gautam/logindemo/ProfileFragment.java
@@ -0,0 +1,13 @@
+package com.imstar.gautam.logindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_fragment);
+    }
+}
